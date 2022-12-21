e4aacf0b51b5e8d1b1d0b69ee5ffce591509c527
gr-eggory <greggroks@gmail.com>
2022-12-20 22:31:38 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
+	import { randomAlphaString, generateChildren, findBestChild, Child, similarity, prettifyPercent } from './util';
+
+	let input = '';
+	let optimal = 'methinks it is like a weasel';
+	let mutationRate = 0.01;
+	let offspring = 15;
+	let generation = 0;
+	
+	let closestIndividual = randomAlphaString(optimal.length);
+	let currentSimilarity = similarity(closestIndividual, optimal);
+	let children = generateChildren(closestIndividual, mutationRate, offspring);
+
+	const assignOptimal = () => {
+		optimal = input;
+	};
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			const newBest = findBestChild(children, optimal);
+			closestIndividual = newBest.text;
+			currentSimilarity = newBest.closeenessToOptimal;
+			children = generateChildren(closestIndividual, mutationRate, offspring);
+			if (closestIndividual === optimal) {
+				clearInterval(interval);
+			}
+			generation++;
+		}, 10);
+
+		onDestroy(() => {
+			clearInterval(interval);
+		});
+	});
+</script>
+
+<main>
+	<div class="output-grid">
+		<h3 class="optimal__label">Optimal:</h3>
+		<h2 class="optimal__value">{optimal}</h2>
+		<h3 class="success__label">Most successful child:</h3>
+		<h2 class="success__value">{closestIndividual}</h2>
+		<h4>Similarity: {prettifyPercent(currentSimilarity)}</h4>
+		<h3 class="generation">Generation: {generation}</h3>
+	</div>
+	<input type="text" bind:value={input} on:submit={assignOptimal} />
+	<button>Begin Simulation</button>
+</main>
+
+<style>
+	main {
+		min-height: 100vh;
+		background-color: whitesmoke;
+		padding-top: 5rem;
+	}
+
+	.output-grid {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		width: 95%;
+		margin: 0 auto;
+		gap: 1rem;
+	}
+
+	.optimal__label {
+		grid-column: 1;
+	}
+
+	.optimal__value {
+		grid-column: 2;
+	}
+
+	.success__label {
+		grid-column: 1;
+	}
+
+	.generation {
+		grid-column: 2;
+	}
+</style>
---
+++ src/util/index.ts
@@ -0,0 +1,72 @@
+import { alphabet } from '../constants.ts';
+
+export const randomAlphaString = (
+	length: number,
+	includeSpaces = true
+): string => {
+	let builtString = '';
+	for (let i = 0; i < length; i++) {
+		builtString +=
+			alphabet[
+				Math.floor(
+					Math.random() *
+						(includeSpaces ? alphabet.length : alphabet.length - 1)
+				)
+			];
+	}
+	return builtString;
+};
+
+export const similarity = (a: string, b: string): number => {
+	const aIsSmaller = a.length - b.length;
+	const smallerLength = aIsSmaller ? a.length : b.length;
+	const largerLength = aIsSmaller ? b.length : a.length;
+	let equivalencies = 0;
+	for (let i = 0; i < smallerLength; i++) {
+		if (a[i] === b[i]) {
+			equivalencies++;
+		}
+	}
+	return equivalencies / largerLength;
+};
+
+export class Child {
+	text: string;
+	closeenessToOptimal: number;
+	constructor(text: string, closeenessToOptimal: number) {
+		this.text = text;
+		this.closeenessToOptimal = closeenessToOptimal;
+	}
+}
+
+export const findBestChild = (children: string[], optimal: string) => {
+	let bestChild = new Child(children[0], similarity(children[0], optimal));
+	for (let i = 1; i < children.length; i++) {
+		const currentChild = children[i];
+		const closenessToOpt = similarity(currentChild, optimal);
+		if (closenessToOpt > bestChild.closeenessToOptimal) {
+			bestChild = new Child(currentChild, closenessToOpt);
+		}
+	}
+	return bestChild;
+};
+
+export const generateChildren = (
+	closestIndividual: string,
+	mutationRate: number,
+	amountOfChildren = 6
+): string[] => {
+	const children: string[] = [];
+	for (let i = 0; i < amountOfChildren; i++) {
+		const newChild = closestIndividual.split('').reduce((acc, letter) => {
+			const shouldMutate = Math.random() < mutationRate;
+			acc += shouldMutate ? randomAlphaString(1) : letter;
+			return acc;
+		}, '');
+		children.push(newChild);
+	}
+	return children;
+};
+
+export const prettifyPercent = (percent: number): string =>
+	`${(percent * 100).toString().substring(0, 4)}%`;
